683fa6b3ac16ca6b483f93f7ec0d67961a0f5f33
Rsccpp <rohitsinghsstc2812@gmail.com>
2025-03-15 12:38:06 +0530

    Initial change

--- Chapters/tempCodeRunnerFile.java
+++ Chapters/tempCodeRunnerFile.java
@@ -1,19 +1,7 @@
-rt java.io.*;
-// public class chp9 {
+package Chapters;
 
-//     public static void main(String[] args){
-//         FileInputStream infile = null;
-//         int b;
-//         try{
-//             infile = new FileInputStream("city.txt");
-//             while ((b = infile.read()) != -1){
-//                 System.out.print((char) b );
-//             }
-//             System.out.println();
-//             infile.close();
-//         }
-//         catch(IOException ioe){
-//            System.out.println(ioe);
-//         }
-//     }
-// }
+public class chp10 {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
