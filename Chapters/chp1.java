02f5f29619cacd93beddb1bb25a4422c99965efa
Rsccpp <rohitsinghsstc2812@gmail.com>
2025-03-18 19:44:47 +0530

    Modification

--- Chapters/chp1.java
+++ Chapters/chp1.java
@@ -4,26 +4,26 @@ package Chapters;
 import java.util.*;
 import java.io.*;
 
-// public class  chp1 {              // File name
+// public class chp1 { // File name
 
-//     public static void main (String [] args) {
+// public static void main (String [] args) {
 
-//  int a = 10;
-//  int b = 20;
-//  int c = a + b;
+// int a = 10;
+// int b = 20;
+// int c = a + b;
 
-//  System.out.println(c);
+// System.out.println(c);
 
-//  String str = new String("Hello World");
-//  System.out.println(str);
+// String str = new String("Hello World");
+// System.out.println(str);
 
-//  // Array
-//  int[] arr = new int[3];
-//  arr[0] = 97;
-//  arr[1] = 98;
-//  arr[2] = 99;
+// // Array
+// int[] arr = new int[3];
+// arr[0] = 97;
+// arr[1] = 98;
+// arr[2] = 99;
 
-//  System.out.println(arr[0]);
+// System.out.println(arr[0]);
 
 // HomeWork Problem
 
@@ -40,93 +40,93 @@ import java.io.*;
 
 // 2. Print Table
 
-//         Scanner sc = new Scanner(System.in);
-//         System.out.println("Enter the number: ");
-//         int num = sc.nextInt();
+// Scanner sc = new Scanner(System.in);
+// System.out.println("Enter the number: ");
+// int num = sc.nextInt();
 
-//         System.out.println("The table of " + num + " is : \n");
+// System.out.println("The table of " + num + " is : \n");
 
-//         for(int i = 1; i<=10; i++) {
-//             int pr = num * i;
+// for(int i = 1; i<=10; i++) {
+// int pr = num * i;
 
-//             System.out.println( num + " * " + i + " = " + pr);
-//         }
-//     }
+// System.out.println( num + " * " + i + " = " + pr);
+// }
+// }
 
 // }
 
 // Strings -- Java Strings are immutable
 
-public class chp1 {
+// public class chp1 {
 
-        public static void main(String[] args) {
-                // Declaration :
-                // String name = "Tony";
+// public static void main(String[] args) {
+// Declaration :
+// String name = "Tony";
 
-                // Taking Input:
-                // Scanner sc = new Scanner(System.in);
-                // String name = sc.next();
+// Taking Input:
+// Scanner sc = new Scanner(System.in);
+// String name = sc.next();
 
-                // System.out.println("Entered name: " + name); // Entered name: Raunak
+// System.out.println("Entered name: " + name); // Entered name: Raunak
 
-                // Concatenation of String
+// Concatenation of String
 
-                // String firstname = "Tony";
-                // String lastname = "Stark";
+// String firstname = "Tony";
+// String lastname = "Stark";
 
-                // String fullname = firstname + ' ' + lastname; // Tony Stark
-                // System.out.println(fullname);
+// String fullname = firstname + ' ' + lastname; // Tony Stark
+// System.out.println(fullname);
 
-                // Length of String
-                // System.out.println(fullname.length()); // 10
+// Length of String
+// System.out.println(fullname.length()); // 10
 
-                // Access characters of a String
-                // for(int i=0; i<fullname.length(); i++){
-                // System.out.print(fullname.charAt(i) + " "); // T o n y S t a r k
-                // }
+// Access characters of a String
+// for(int i=0; i<fullname.length(); i++){
+// System.out.print(fullname.charAt(i) + " "); // T o n y S t a r k
+// }
 
-                // Compare 2 strings // They are different strings
+// Compare 2 strings // They are different strings
 
-                // if(firstname.equals(lastname)){
-                // System.out.println("They are the same string.");
-                // } else {
-                // System.out.println("They are different strings");
-                // }
+// if(firstname.equals(lastname)){
+// System.out.println("They are the same string.");
+// } else {
+// System.out.println("They are different strings");
+// }
 
-                // Do not use == to check for string equality
+// Do not use == to check for string equality
 
-                // Gives correct answer here // They are different strings
-                // if(firstname == lastname){
-                // System.out.println("They are the same string.");
-                // } else {
-                // System.out.println("They are different strings");
-                // }
+// Gives correct answer here // They are different strings
+// if(firstname == lastname){
+// System.out.println("They are the same string.");
+// } else {
+// System.out.println("They are different strings");
+// }
 
-                // Gives incorrect answer here // They are different strings
-                // if(new String("Tony") == new String("Tony") ) {
-                // System.out.println("They are the same string");
-                // } else {
-                // System.out.println("They are different strings");
-                // }
+// Gives incorrect answer here // They are different strings
+// if(new String("Tony") == new String("Tony") ) {
+// System.out.println("They are the same string");
+// } else {
+// System.out.println("They are different strings");
+// }
 
-                // Substring
+// Substring
 
-                // String name = "Tony Stark";
-                // System.out.println(name.substring(0, 4)); // Tony
+// String name = "Tony Stark";
+// System.out.println(name.substring(0, 4)); // Tony
 
-                // ParseInt Method of Integer class
-                // String str = "123";
-                // int number = Integer.parseInt(str);
-                // System.out.println((number));
+// ParseInt Method of Integer class
+// String str = "123";
+// int number = Integer.parseInt(str);
+// System.out.println((number));
 
-                // ToString Method of String class
-                int number = 123;
-                String str = Integer.toString(number);
-                System.out.println(str.length()); // 3
-                System.out.println(str.getClass().getName()); // java.lang.String
+// ToString Method of String class
+// int number = 123;
+// String str = Integer.toString(number);
+// System.out.println(str.length()); // 3
+// System.out.println(str.getClass().getName()); // java.lang.String
 
-        }
-}
+// }
+// }
 
 // Problems:
 
@@ -188,26 +188,93 @@ public class chp1 {
 // 3. Input an email from the user. You have to create a username from the email
 // by deleting the part that comes after '@'
 
-public class chp1 {
+// public class chp1 {
 
-        public static void main(String[] args) {
-                Scanner sc = new Scanner(System.in);
-                System.out.println("Enter email: ");
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// System.out.println("Enter email: ");
 
-                String email = sc.next();
-                String userName = " ";
+// String email = sc.next();
+// String userName = " ";
 
-                for (int i = 0; i < email.length(); i++) {
-                        if (email.charAt(i) == '@') {
-                                break;
-                        } else {
-                                userName += email.charAt(i);
-                        }
-                }
-                System.out.println("Entered Email: " + email);
-                System.out.println("Usename: " + userName);
+// for (int i = 0; i < email.length(); i++) {
+// if (email.charAt(i) == '@') {
+// break;
+// } else {
+// userName += email.charAt(i);
+// }
+// }
+// System.out.println("Entered Email: " + email);
+// System.out.println("Usename: " + userName);
 
-        }
-}
+// }
+// }
 // Entered Email: rohitsingh@gmail.com
 // Usename: rohitsingh
+
+// **************** StringBuilder -- Use to make String mutable *****
+
+// public class chp1 {
+
+// public static void main(String[] args) {
+// // Declaration
+// StringBuilder sb = new StringBuilder("Java Journey");
+// System.out.println(sb);
+
+// // Get a character from Index
+// System.out.println(sb.charAt(0));
+
+// // Set a character at Index
+// sb.setCharAt(0, 'j');
+// System.out.println(sb);
+
+// // Insert a character at some index
+// sb.insert(0, 'S');
+// System.out.println(sb);
+
+// // Delete character from some index
+// sb.delete(0, 1);
+// System.out.println(sb);
+
+// // Append a char -- Means to add something at the end
+// sb.append('!');
+// System.out.println(sb);
+
+// // Print length of String
+// System.out.println(sb.length());
+// }
+// }
+
+// o/p:
+// Java Journey
+// J
+// java Journey
+// Sjava Journey
+// java Journey
+// java Journey!
+// 13
+
+// 1. Reverse a String (using StringBuilder class)
+// public class chp1 {
+
+// public static void main(String[] args) {
+// StringBuilder sb = new StringBuilder("Hello World");
+
+// System.out.println("Befor Reverse String: " + sb);
+// for (int i = 0; i < sb.length() / 2; i++) {
+// int front = i;
+// int back = sb.length() - i - 1;
+
+// char frontChar = sb.charAt(front);
+// char backChar = sb.charAt(back);
+
+// sb.setCharAt(front, backChar);
+// sb.setCharAt(back, frontChar);
+// }
+// System.out.println("After Reverse String: " + sb);
+// }
+// }
+
+// o/p:
+// Befor Reverse String: Hello World
+// After Reverse String: dlroW olleH
