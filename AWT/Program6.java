4efa9698cf4ce36577e1dbf6adab0bfc4b6c6ed2
Rsccpp <rohitsinghsstc2812@gmail.com>
2025-03-28 21:49:09 +0530

    Modification

--- AWT/Program6.java
+++ AWT/Program6.java
@@ -11,8 +11,8 @@ package AWT;
 
 
 
-import java.awt.*;
-import java.awt.event.*;
+// import java.awt.*;
+// import java.awt.event.*;
 
 // Example 1 : 
 
@@ -77,3 +77,41 @@ import java.awt.event.*;
 //            new Program6();
 //        }
 // }
+
+
+// Action Listener
+import java.awt.*;
+import java.awt.event.*;
+
+//1st step
+public class Program6 implements ActionListener{
+    Frame f;
+    TextField tf;
+    Button b;
+
+    Program6(){
+        f=new Frame("ActionListener Example");
+        tf= new TextField();
+        tf.setBounds(50, 50, 300, 20);
+        b= new Button ("Click Here");
+        b.setBounds(50, 100, 70, 30);
+
+        //2nd step
+        b.addActionListener(this);
+        f.add(b);
+        f.add(tf);
+        f.setSize(400, 400);
+        f.setLayout(null);
+        f.setVisible(true);
+
+    }
+    //3rd step
+    public void actionPerformed(ActionEvent ae){
+        tf.setText("Welcome to the Event Handling Program");
+    }
+
+    public static void main(String[] args) {
+        Program6 ae = new Program6();
+        //new Program6();
+    }
+}
